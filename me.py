6359109eaf17efb5bdb700471964d4d6d80a19ae
aniruddh <aniruddh@appointy.com>
2019-06-25 12:46:28 +0530

    adding square functionality

--- me.py
+++ me.py
@@ -3,6 +3,9 @@ import time
 import pyautogui 
 while True:
     time.sleep(1)   
-    pyautogui.moveRel(1000, 100, duration = 1) 
-    pyautogui.moveRel(-1000, 0, duration = 1) 
+    pyautogui.moveRel(1000, 0, duration = 5) 
+    pyautogui.moveRel(0, 500, duration = 5) 
+    pyautogui.moveRel(-1000, 0, duration = 5) 
+    pyautogui.moveRel(0, -500, duration = 5) 
+
 
