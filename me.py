2b3da6ac186554e5c5cf3b7f89937d3ec52b0022
aniruddh <aniruddh@appointy.com>
2019-06-25 13:13:02 +0530

    adding the tab swithching feature

--- me.py
+++ me.py
@@ -2,9 +2,11 @@
 import time 
 import pyautogui 
 while True:
+    time.sleep(30)
     pyautogui.moveRel(10, 0, duration = .05) 
     pyautogui.moveRel(0, 5, duration = .05) 
     pyautogui.moveRel(-10, 0, duration = .05) 
     pyautogui.moveRel(0, -5, duration = .05) 
+    pyautogui.hotkey("altleft", "tab") 
 
 
