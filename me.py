d08291d760e048fa21547124e76715b6bf9c497f
aniruddh <aniruddh@appointy.com>
2019-06-25 12:56:56 +0530

    making the expreiance better and increasing the speed

--- me.py
+++ me.py
@@ -2,10 +2,9 @@
 import time 
 import pyautogui 
 while True:
-    time.sleep(1)   
-    pyautogui.moveRel(1000, 0, duration = 5) 
-    pyautogui.moveRel(0, 500, duration = 5) 
-    pyautogui.moveRel(-1000, 0, duration = 5) 
-    pyautogui.moveRel(0, -500, duration = 5) 
+    pyautogui.moveRel(10, 0, duration = .05) 
+    pyautogui.moveRel(0, 5, duration = .05) 
+    pyautogui.moveRel(-10, 0, duration = .05) 
+    pyautogui.moveRel(0, -5, duration = .05) 
 
 
